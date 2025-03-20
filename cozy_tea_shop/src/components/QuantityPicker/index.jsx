ffc0cc89382cf4de39ebf835cb1099c677173b87
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-03-20 00:34:22 -0500

    Add index.jsx for QuantityPicker to support simplified and consistent imports

---
+++ cozy_tea_shop/src/components/QuantityPicker/index.jsx
@@ -0,0 +1,3 @@
+// Re-exporting the QuantityPicker component so I can import it cleanly from the folder.
+// This helps me keep my import statements short and consistent throughout the app.
+export { default } from './QuantityPicker';
