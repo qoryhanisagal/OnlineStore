7ade3f973c79a0861c14c20e9796d67a8f3b3c72
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-03-22 16:13:39 -0500

    Built basic Cart page with placeholder layout and styles

---
+++ cozy_tea_shop/src/pages/Cart/cart.jsx
@@ -0,0 +1,9 @@
+// Import React
+// Import Cart.css
+
+function Cart() {
+    // Return a div with className "cart-page"
+    // Inside, render <h1>Your Shopping Cart</h1>
+  }
+  
+  export default Cart;
