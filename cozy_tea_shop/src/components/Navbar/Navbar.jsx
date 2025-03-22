11babfbaddc8385f5f029f546feb8c487fab957a
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-03-22 16:12:42 -0500

    Updated Navbar structure and styling for improved layout

--- cozy_tea_shop/src/components/Navbar/Navbar.jsx
+++ cozy_tea_shop/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
-import "./Navbar.css";
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
+  const { cartItems } = useContext(CartContext); // Grabs cart data
+
+  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0); // Total quantity
+
   return (
     <div className="navbar">
       <span className="title">Cozy<br />Tea Shop</span>
@@ -11,6 +17,12 @@ function Navbar() {
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/admin">Admin</Link></li>
+        <li>
+          <Link to="/cart" className="cart-link">
+            <i className="bi bi-bag"></i> {/* Bootstrap bag icon */}
+            {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+          </Link>
+        </li>
       </ul>
     </div>
   );
